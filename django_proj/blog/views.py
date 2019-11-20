4c90caccdfd173d645100cec4a04c15dcc228c7f
The Sarge <sergantspam@gmail.com>
2019-11-20 15:54:17 +0200

    change to posts from db

--- django_proj/blog/views.py
+++ django_proj/blog/views.py
@@ -1,28 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime, date
-
-posts = [
-    {
-        'author': 'Yaakov Shore',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': date(2018, 7, 5)
-    },
-    {
-        'author': 'Chani Shore',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': date(2019, 7, 5)
-    }
-]
-
-# Create your views here.
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
